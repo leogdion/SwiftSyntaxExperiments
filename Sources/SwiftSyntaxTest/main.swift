1c49c6885eadd9cb085a7f30b862ca32265ecaac
Leo Dion <leogdion@brightdigit.com>
2020-06-20 14:34:33 -0400

    working with PackageConfig

--- Sources/SwiftSyntaxTest/main.swift
+++ Sources/SwiftSyntaxTest/main.swift
@@ -1,17 +1,23 @@
 import SwiftSyntax
 
+
 let printIdentiferExpr = ExprSyntax(
   SyntaxFactory.makeIdentifierExpr(
-    identifier: SyntaxFactory.makeIdentifier("print"),
+    identifier: SyntaxFactory.makeIdentifier("canImport"),
     declNameArguments: nil))
-let exprParameter = ExprSyntax(SyntaxFactory.makeStringLiteralExpr("Hello World"))
-let tupleArg = SyntaxFactory.makeTupleExprElement(
-  label: nil, colon: nil, expression: exprParameter, trailingComma: nil)
-let funcCall = SyntaxFactory.makeFunctionCallExpr(
-  calledExpression: printIdentiferExpr,
-  leftParen: SyntaxFactory.makeLeftParenToken(),
-  argumentList: SyntaxFactory.makeTupleExprElementList([tupleArg]),
-  rightParen: SyntaxFactory.makeRightParenToken(),
-  trailingClosure: nil)
 
-print(funcCall)
+let exprParameter = SyntaxFactory.makeToken(.contextualKeyword("PackageConfig"), presence: .present)
+let accessPath = SyntaxFactory.makeAccessPath([])
+
+let syntax = SyntaxFactory.makeImportDecl(attributes: nil, modifiers: nil, importTok: exprParameter, importKind: nil, path: accessPath)
+//let tupleArg = SyntaxFactory.makeTupleExprElement(
+//  label: nil, colon: nil, expression: exprParameter, trailingComma: nil)
+//let funcCall = SyntaxFactory.makeFunctionCallExpr(
+//  calledExpression: printIdentiferExpr,
+//  leftParen: SyntaxFactory.makeLeftParenToken(),
+//  argumentList: SyntaxFactory.makeTupleExprElementList([tupleArg]),
+//  rightParen: SyntaxFactory.makeRightParenToken(),
+//  trailingClosure: nil)
+
+//let ifClause = SyntaxFactory.makeIfConfigClause(poundKeyword: SyntaxFactory.makePoundIfKeyword(), condition: nil, elements: funcCall)
+print(syntax)
